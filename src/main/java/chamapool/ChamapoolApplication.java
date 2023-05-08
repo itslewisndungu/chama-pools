729a35c6caa5951d270e36437bc66cff6dab4013
itslewisndungu <itslewisndungu@gmail.com>
2023-05-08 11:32:26 +0300

    Initial setup with `spring.start.io`

---
+++ src/main/java/chamapool/ChamapoolApplication.java
@@ -0,0 +1,13 @@
+package chamapool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamapoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamapoolApplication.class, args);
+	}
+
+}
