048a739cf4242264591c71eb7b01094106ceb1c5
itslewisndungu <itslewisndungu@gmail.com>
2023-06-05 18:55:22 +0300

    feat: Encode user roles into the jwt claims

--- src/main/java/chamapool/application/auth/JwtService.java
+++ src/main/java/chamapool/application/auth/JwtService.java
@@ -1,30 +1,31 @@
 package chamapool.application.auth;
 
 import chamapool.domain.member.models.Member;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-
 @Service
 @RequiredArgsConstructor
 public class JwtService {
-    private final JwtEncoder encoder;
+  private final JwtEncoder encoder;
 
-    public String generateToken(Member user) {
-        Instant now = Instant.now();
+  public String generateToken(Member user) {
+    Instant now = Instant.now();
 
-        JwtClaimsSet claims = JwtClaimsSet.builder()
-                .issuer("self")
-                .issuedAt(now)
-                .expiresAt(now.plus(2, ChronoUnit.HOURS))
-                .subject(user.username())
-                .build();
+    JwtClaimsSet claims =
+        JwtClaimsSet.builder()
+            .issuer("self")
+            .issuedAt(now)
+            .expiresAt(now.plus(2, ChronoUnit.HOURS))
+            .subject(user.username())
+            .claim("roles", user.roles().toString())
+            .build();
 
-        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-    }
+    return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+  }
 }
