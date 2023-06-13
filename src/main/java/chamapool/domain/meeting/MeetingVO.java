40aa09cdb14b1828c3ea6c49a4f9ed5aab697d06
itslewisndungu <itslewisndungu@gmail.com>
2023-06-13 05:54:58 +0300

    Create a MeetingVO to return meeting details

--- src/main/java/chamapool/domain/meeting/MeetingVO.java
+++ src/main/java/chamapool/domain/meeting/MeetingVO.java
@@ -1,3 +1,22 @@
 package chamapool.domain.meeting;
 
-public record MeetingVO() {}
+import chamapool.domain.meeting.enums.MeetingKind;
+import chamapool.domain.meeting.enums.MeetingPresence;
+import chamapool.domain.meeting.models.Meeting;
+import java.time.LocalDate;
+
+public record MeetingVO(
+    Integer id, LocalDate date, MeetingKind kind, String agenda, Integer attendanceCount) {
+  public MeetingVO(Meeting meeting) {
+    this(
+        meeting.meetingId(),
+        meeting.date(),
+        meeting.kind(),
+        meeting.agenda(),
+        meeting.meetingAttendanceList().stream()
+            .filter(
+                meetingAttendance -> meetingAttendance.presence().equals(MeetingPresence.PRESENT))
+            .toList()
+            .size());
+  }
+}
