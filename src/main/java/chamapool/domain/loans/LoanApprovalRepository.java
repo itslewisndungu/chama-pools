1baaf7f918d0a4a4bb677f8e537d2c0fa338010a
itslewisndungu <itslewisndungu@gmail.com>
2023-06-13 11:18:59 +0300

    Create repository for `LoanRepayment`, `LoanApproval` and the `Loan` entities

---
+++ src/main/java/chamapool/domain/loans/LoanApprovalRepository.java
@@ -0,0 +1,5 @@
+package chamapool.domain.loans;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanApprovalRepository extends JpaRepository<LoanApproval, Integer> {}
