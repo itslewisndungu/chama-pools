cb0c675563ffb9224c474d259751ca2610c35b4a
itslewisndungu <itslewisndungu@gmail.com>
2023-06-24 21:06:57 +0300

    Append applicant information on the `LoanApplicationVO`

--- src/main/java/chamapool/domain/loans/VO/LoanApplicationVO.java
+++ src/main/java/chamapool/domain/loans/VO/LoanApplicationVO.java
@@ -4,10 +4,19 @@ import chamapool.domain.loans.LoanApplication;
 import chamapool.domain.loans.enums.LoanApprovalStatus;
 
 public record LoanApplicationVO(
-    Double amount, LoanApprovalStatus status, String reasonForLoan, Approvals approval) {
+    Double amount,
+    Integer memberId,
+    String memberName,
+    String memberPhoneNumber,
+    LoanApprovalStatus status,
+    String reasonForLoan,
+    Approvals approval) {
   public LoanApplicationVO(LoanApplication application, Approvals approvals) {
     this(
         application.amount(),
+        application.member().id(),
+        application.member().fullName(),
+        application.member().phoneNumber(),
         application.approvalStatus(),
         application.reasonForLoan(),
         approvals);
