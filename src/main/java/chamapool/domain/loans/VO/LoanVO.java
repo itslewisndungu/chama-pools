705142f156ec02923e740c72143e232fc9695528
itslewisndungu <itslewisndungu@gmail.com>
2023-06-26 17:55:02 +0300

    Use new `Loan` dueDate

--- src/main/java/chamapool/domain/loans/VO/LoanVO.java
+++ src/main/java/chamapool/domain/loans/VO/LoanVO.java
@@ -7,26 +7,28 @@ import java.time.LocalDate;
 public record LoanVO(
     Integer id,
     Integer memberId,
+    String memberName,
     Double amount,
     Double interestEarned,
     Double amountPayable,
     String reasonForLoan,
     Double interestRate,
     LocalDate startDate,
-    LocalDate expectedEndDate,
+    LocalDate dueDate,
     LocalDate endDate,
     LoanStatus status) {
   public LoanVO(Loan loan) {
     this(
         loan.id(),
         loan.member().id(),
+        loan.member().fullName(),
         loan.amount(),
         loan.interestEarned(),
         loan.amountPayable(),
         loan.reasonForLoan(),
         loan.interestRate(),
         loan.startDate(),
-        loan.expectedEndDate(),
+        loan.dueDate(),
         loan.endDate(),
         loan.status());
   }
