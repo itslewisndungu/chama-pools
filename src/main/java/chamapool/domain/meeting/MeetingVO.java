fd5f9ac8734daa46f31f98c65e5b4f8133898e6d
itslewisndungu <itslewisndungu@gmail.com>
2023-06-27 19:01:10 +0300

    Use `lambda` for filter function

--- src/main/java/chamapool/domain/meeting/MeetingVO.java
+++ src/main/java/chamapool/domain/meeting/MeetingVO.java
@@ -1,21 +1,20 @@
 package chamapool.domain.meeting;
 
 import chamapool.domain.meeting.enums.MeetingKind;
-import chamapool.domain.meeting.enums.MeetingPresence;
 import chamapool.domain.meeting.models.Meeting;
+import chamapool.domain.meeting.models.MeetingAttendance;
 import java.time.LocalDate;
 
 public record MeetingVO(
-    Integer id, LocalDate date, MeetingKind kind, String agenda, Integer attendanceCount) {
+    Integer id, LocalDate date, MeetingKind kind, String agenda, Integer membersPresent) {
   public MeetingVO(Meeting meeting) {
     this(
         meeting.meetingId(),
-        meeting.date(),
+        meeting.meetingDate(),
         meeting.kind(),
         meeting.agenda(),
         meeting.meetingAttendanceList().stream()
-            .filter(
-                meetingAttendance -> meetingAttendance.presence().equals(MeetingPresence.PRESENT))
+            .filter(MeetingAttendance::isPresent)
             .toList()
             .size());
   }
