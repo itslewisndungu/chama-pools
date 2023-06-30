ebf3dd2c0382fc112eda4c08e0b92704bae9360e
itslewisndungu <itslewisndungu@gmail.com>
2023-06-30 22:20:17 +0300

    Create Transaction entity

---
+++ src/main/java/chamapool/domain/transaction/Transaction.java
@@ -0,0 +1,18 @@
+package chamapool.domain.transaction;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Setter
+@Getter
+@Accessors(chain = true, fluent = true)
+@EntityListeners(AuditingEntityListener.class)
+public class Transaction {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Integer id;
+}
---
+++ src/main/java/chamapool/domain/transaction/TransactionType.java
@@ -0,0 +1,11 @@
+package chamapool.domain.transaction;
+
+public enum TransactionType {
+  DEPOSIT,
+  WITHDRAWAL,
+  LOAN_DISBURSEMENT,
+  LOAN_REPAYMENT,
+  LOAN_INTEREST,
+  CONTRIBUTION,
+  MEMBERSHIP_FEE,
+}
