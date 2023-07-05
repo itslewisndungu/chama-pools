4642c42bf1e543f53b3d7301f7749ca54949ddaf
itslewisndungu <itslewisndungu@gmail.com>
2023-07-05 07:16:27 +0300

    refactor: Create methods for retrieving admins

--- src/main/java/chamapool/domain/member/repositories/MemberRepository.java
+++ src/main/java/chamapool/domain/member/repositories/MemberRepository.java
@@ -1,8 +1,6 @@
 package chamapool.domain.member.repositories;
 
-import chamapool.domain.member.enums.MemberRole;
 import chamapool.domain.member.models.Member;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +8,12 @@ import org.springframework.data.jpa.repository.Query;
 public interface MemberRepository extends JpaRepository<Member, Integer> {
   Optional<Member> getMemberByUsername(String username);
 
-  @Query("SELECT m FROM Member m WHERE ?1 in (SELECT r.name FROM m.roles r) ")
-  List<Member> getMembersByRole(MemberRole role);
+  @Query("SELECT m FROM Member m WHERE 'CHAIRMAN' in (SELECT r.name FROM m.roles r) ")
+  Optional<Member> findChairman();
+
+  @Query("SELECT m FROM Member m WHERE 'SECRETARY' in (SELECT r.name FROM m.roles r) ")
+  Optional<Member> findSecretary();
+
+  @Query("SELECT m FROM Member m WHERE 'TREASURER' in (SELECT r.name FROM m.roles r) ")
+  Optional<Member> findTreasurer();
 }
