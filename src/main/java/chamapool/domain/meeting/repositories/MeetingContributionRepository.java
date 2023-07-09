055aade220f4ef2ace4860ecb40af8b9ff5f4f3e
itslewisndungu <itslewisndungu@gmail.com>
2023-07-09 11:33:14 +0300

    Create custom query to group contributions per member

--- src/main/java/chamapool/domain/meeting/repositories/MeetingContributionRepository.java
+++ src/main/java/chamapool/domain/meeting/repositories/MeetingContributionRepository.java
@@ -6,9 +6,20 @@ import chamapool.domain.member.models.Member;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MeetingContributionRepository extends JpaRepository<MeetingContribution, Integer> {
     Optional<MeetingContribution> getMeetingContributionByMemberAndMeeting(Member member, Meeting meeting);
 
     List<MeetingContribution> getMeetingContributionsByMember(Member member);
+
+  @Query(value = """
+              select m.member.id as id,
+              m.member.firstName as firstName,
+              m.member.lastName as lastName,
+              sum(m.amount) as totalContributions from MeetingContribution as m
+              GROUP BY  m.member.id,  m.member.firstName, m.member.lastName
+              order by totalContributions desc
+          """)
+  List<IContributionsSum> getMeetingContributionsGroupByMember();
 }
